dd15d8f882f3ee3d0e0622ecbcc6ec7d910a6373
patrick <patrick>
2026-08-24 20:06:00 -0400

    Eliminacion de comentarios base de principal realizado por Rodriguez Patrick

--- src/githubnumparimpar/GitHubNumParImpar.java
+++ src/githubnumparimpar/GitHubNumParImpar.java
@@ -1,23 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package githubnumparimpar;
-
 import java.util.Scanner;
-
-/**
- *
- * @author hp
- */
 public class GitHubNumParImpar {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //modificado por Rodriguez Patrick
         utilidades util =new utilidades();
         int [] pares=new int [5];
         int [] impares=new int [5];
